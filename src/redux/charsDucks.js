dd9671e0c9a9b9a03219dd933a37dce7c62126dd
frezeer <frezee13@hotmail.com>
2020-07-18 10:12:08 -0500

    reduce trabajando antes de borrar home

--- src/redux/charsDucks.js
+++ src/redux/charsDucks.js
@@ -7,7 +7,6 @@ let initialData = {
     current: {}
 };
 
-
 let URL = "https://rickandmortyapi.com/api/character"
 
 let GET_CHARACTERS = "GET_CARACTERS"
@@ -19,9 +18,11 @@ let GET_CHARACTERS_ERROR = "GET_CARACTERS_ERROR"
 export default function reducer(state=initialData, action){
     switch(action.type){
         case GET_CHARACTERS:
-        case GET_CHARACTERS_SUCCES:    
-            return { ...state, array: action.payload}
+            return { ...state, fetching: true }
+        case GET_CHARACTERS_SUCCES:   
+            return { ...state, fetching: false , error:action.payload }
         case GET_CHARACTERS_ERROR:
+            return { ...state, array: action.payload, fetching: false}
         default:
             return state
     }
@@ -29,13 +30,22 @@ export default function reducer(state=initialData, action){
 
 //actions(thunks)
 export let getCharactersActions = () => (dispatch, getState) => {
+    dispatch({
+        type: GET_CHARACTERS
+    })
     return axios.get(URL)
     .then(res => {
         dispatch({
             type: GET_CHARACTERS_SUCCES,
             payload: res.data.results
         })
-    });
+    })
+    .catch(err=>{
+        dispatch({
+        type: GET_CHARACTERS_ERROR,
+        payload: err.response.message
+        })
+    })
 }
 
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -7,17 +7,16 @@ import charsReducer, { getCharactersActions } from './charsDucks';
 let rootReducer = combineReducers({
     user : userReducer,
     characters: charsReducer
-   
 })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 
 export default function generateStore(){
     let store = createStore(
         rootReducer,
         composeEnhancers(applyMiddleware(thunk))
         )
+        //los mandamos llamar a los persoanjes por primera vez
             getCharactersActions()(store.dispatch,store.getState)
      return store   
 }
